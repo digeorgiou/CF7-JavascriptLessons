0cdd40255c0802778d28431ba302af71d15574ae
Dimitris Georgiou <digeorgiou1990@gmail.com>
2025-02-25 17:27:38 +0200

    added some challenges

---
+++ challenges/sweetDate/sweetDate.js
@@ -0,0 +1,39 @@
+/**
+ * John and Anne are a pair of lovebirds. They have their own jobs. John works w1 days off for r1 day and Anne works w2 days off for r2 days. Only when they have a rest, they can go out for a sweet date. Please calculate, they can date how many days in a time period? For example:
+
+John works 3 days off for 1 day 
+Anne works 7 days off for 3 days
+In a time period 10 day
+John: work work work rest work work work rest work work
+                                           |
+Anne: work work work work work work work rest rest rest
+They have only one sweet date in 10 days
+
+In a time period 1000 day
+John: work work work rest work work work rest work work ....
+                                           |
+Anne: work work work work work work work rest rest rest ....
+They will have 100 times sweet date in 1000 days
+Task
+Complete function sweetDate() that accepts 5 arguments:
+
+w1,r1: The work days and rest days of John
+
+w2,r2: The work days and rest days of Anne
+
+timePeriod: A time period that we need to calculate the sweet date
+
+Return a number that How many times can Anne and John have a sweet date.
+
+Example
+w1	r1	w2	r2	time_period	Result
+3	1	7	3	10	1
+3	1	7	3	20	2
+4	2	7	3	20	1
+4	2	7	3	30	3
+3	1	7	3	1000	100
+ */
+
+
+
+
---
+++ modules/src/lib2.js
@@ -0,0 +1,11 @@
+//named exports
+const PI = 3.14
+const EPSILON = 0.000005
+const API_URL = "htts://codingfactory.aueb.gr"
+
+//default export - μονο ενα μπορουμε
+const SEC_PER_MIN = 60
+export default SEC_PER_MIN
+
+//τα names exports μπορουν να γραφτουν κι ετσι.
+export {PI, EPSILON,API_URL}
